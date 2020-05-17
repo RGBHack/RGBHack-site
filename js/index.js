fe4f119e42c1dd56616b64a88fc8f7fba9e76dac
raad1masum <piraadwan@gmail.com>
2020-05-17 16:38:02 -0700

    add box shadow to main box

--- js/index.js
+++ js/index.js
@@ -10,3 +10,9 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
