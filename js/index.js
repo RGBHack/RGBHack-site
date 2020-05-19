656021b1895e0bdd15b170476d96f9f1ab451043
raad1masum <piraadwan@gmail.com>
2020-05-18 18:21:37 -0700

    make active actually nav work

--- js/index.js
+++ js/index.js
@@ -8,24 +8,29 @@ function myFunction() {
   }
 }
 
-$(document).ready(function(){
-	$(window).scroll(function () {
-			if ($(this).scrollTop() > 50) {
-				$('#back-to-top').fadeIn();
-			} else {
-				$('#back-to-top').fadeOut();
-			}
-		});
-		// scroll body to 0px on click
-		$('#back-to-top').click(function () {
-			$('body,html').animate({
-				scrollTop: 0
-			}, 400);
-			return false;
-		});
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $("#back-to-top").fadeIn();
+    } else {
+      $("#back-to-top").fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $("#back-to-top").click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      400
+    );
+    return false;
+  });
 });
 
-$(".nav-item").on("click", function(e){
-  $("li.nav-item").removeClass("active");
-  $(this).addClass("active");
+window.addEventListener("hashchange", function () {
+  document.querySelector(".active").classList.remove("active");
+  document
+    .querySelector('[href="' + window.location.hash + '"]')
+    .classList.add("active");
 });
